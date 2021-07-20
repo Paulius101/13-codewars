45bd962f9603804231b2c43e25b490dda0420ab1
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-07-20 19:36:17 +0300

    4 more

---
+++ codewars/noSpaces.js
@@ -0,0 +1,7 @@
+function noSpace(x) {
+    let xSplit = x.split(' ');
+    return xSplit.join("");
+}
+
+// Simple, remove the spaces from the string, then return the resultant string.
+
---
+++ codewars/numberToString.js
@@ -0,0 +1,7 @@
+function numberToString(num) {
+    return num.toString();
+}
+
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know ?
